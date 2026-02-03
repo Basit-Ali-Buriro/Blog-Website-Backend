fda93cf28e12cefb4d353365a177dbfcf13e238c
Basit Ali <basitaliburiro1110@gmail.com>
2026-02-03 18:22:08 +0500

    fixed cors issue

--- server.js
+++ server.js
@@ -40,7 +40,9 @@ const authLimiter = rateLimit({
 const allowedOrigins = [
   process.env.CLIENT_URL,
   'http://localhost:5173',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://www.bestblogs.studio',
+  'https://bestblogs.studio'
 ].filter(Boolean); // Remove undefined/null values
 
 const corsOptions = {
